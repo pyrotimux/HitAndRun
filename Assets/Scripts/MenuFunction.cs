bb604bc4f9a59343c678b834c8ee49da2e914c94
Phyo W. Shwe <chaosmuskey@gmail.com>
2017-08-11 01:04:32 -0700

    Added enviroment elements.

--- Assets/Scripts/MenuFunction.cs
+++ Assets/Scripts/MenuFunction.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class MenuFunction : MonoBehaviour {
 
+    GameObject godirect;
 
+    public void Start()
+    {
+        godirect = GameObject.Find("MainPanel").transform.GetChild(2).gameObject;
+    }
 
     public void Button_Quit()
     {
         Application.Quit();
+    }
+
+    public void Button_Show_DirectPlay() {
+        
+        godirect.SetActive(true);
+    }
+
+    public void Button_Show_Back()
+    {
+       
     }
 }
